d148eaccdddcd29bf7e39ffeffcd8da3fc32ad64
Mert Kahraman <73400722+TheDerebeyi@users.noreply.github.com>
2023-01-08 17:16:37 +0300

    Initial commit

---
+++ src/process/Program.java
@@ -0,0 +1,35 @@
+package process;
+
+import java.util.Formatter;
+/*import java.util.Timer;
+import java.util.TimerTask;*/
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		if(args.length == 0) {
+			System.out.println("Parametre hatasý.");
+			System.exit(0);
+		}
+
+		int saniye = Integer.parseInt(args[0]);
+		int id = Integer.parseInt(args[1]);
+		int oncelik = Integer.parseInt(args[2]);
+		int kalanSure = Integer.parseInt(args[3]);
+		//Timer timer = new Timer();
+		
+		/*timer.schedule( new TimerTask() {
+		    public void run() {*/
+		    	Formatter formatter = new Formatter();
+		    	String s = formatter.format("%d.0000 sn", saniye).toString();
+		    	formatter.close();
+		    	formatter = new Formatter();
+		    	String s2 = formatter.format("%-10s proses yürütülüyor", s).toString();
+				System.out.format("%-30s(id:%04d  oncelik:%d  kalan süre:%d sn)", s2, id,oncelik,kalanSure);	formatter.close();	System.exit(0); /*}}, 1000);*/
+		
+
+
+	}
+
+}
